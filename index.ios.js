63115352bbf129fede53ab34447ed5f5e8f2526c
Calvin Yau <calvinj.yau@gmail.com>
2017-02-05 00:19:21 -0800

    Connect show page to button in home

--- index.ios.js
+++ index.ios.js
@@ -4,6 +4,7 @@ import { AppRegistry, Navigator } from 'react-native';
 import AllPackages from './react-assets/components/all_packages';
 import VerifyPIN from './react-assets/components/verify_pin';
 import Home from './react-assets/components/home';
+import ShowPackage from './react-assets/components/show_package';
 
 class Shiparoo extends Component {
   constructor(props) {
@@ -24,6 +25,8 @@ class Shiparoo extends Component {
      return <AllPackages navigator={navigator} />;
    } else if (route.name === 'Verify PIN') {
      return <VerifyPIN navigator={navigator} />;
+   } else if (route.name === 'Show Package') {
+     return <ShowPackage navigator={navigator} {...route.passProps} />;
    }
   }
 
